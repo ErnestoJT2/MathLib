c96e0aa0ccafd922b8232a2182c94d22070079b2
A01754887 Ernesto Juárez Torres <A01754887@itesm.mx>
2025-02-05 17:38:34 -0600

    ok

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ernes/OneDrive/Documentos/MathLib
+# Build directory: C:/Users/ernes/OneDrive/Documentos/MathLib/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[AllTests]=] "C:/Users/ernes/OneDrive/Documentos/MathLib/cmake-build-debug/test_suite.exe")
+set_tests_properties([=[AllTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ernes/OneDrive/Documentos/MathLib/CMakeLists.txt;39;add_test;C:/Users/ernes/OneDrive/Documentos/MathLib/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
